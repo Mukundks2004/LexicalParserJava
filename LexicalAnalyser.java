0c7a32a2c47b7d71621487902f71a050e8c0e5c1
Mks <mukundsrinivasan0@gmail.com>
2025-01-05 14:17:47 +1100

    return constant tokenlist

--- LexicalAnalyser.java
+++ LexicalAnalyser.java
@@ -1,12 +1,19 @@
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LexicalAnalyser {
 
 	public static List<Token> analyse(String input) throws NumberException, ExpressionException {
+		List<Token> myList = new ArrayList<Token>();
 
-		return Collections.<Token>emptyList();
+		Token t1 = new Token(2);
+		Token t2 = new Token(3.14);
+		Token t3 = new Token(Token.TokenType.DIVIDE);
 
+		myList.add(t1);
+		myList.add(t2);
+		myList.add(t3);
+
+		return myList;
 	}
-
 }
